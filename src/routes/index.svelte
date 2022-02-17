cf9d18a55fc98e5abc21de37b47f50351446bce5
Tommy <tommy.tran@ivsllc.com>
2022-02-17 16:11:45 -0500

    initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+    // @ts-nocheck
+    
+      import { Editor } from '../../node_modules/bytemd';
+      import gfm from '../../node_modules/@bytemd/plugin-gfm/lib/index';
+    
+      import { onMount } from 'svelte'
+      let value = '';
+      const plugins = [
+        gfm(),
+        // Add more plugins here
+      ];
+    
+      function handleChange(e) {
+        value = e.detail.value;
+      }
+    </script>
+    
+    <template>
+      <Editor {value} {plugins} on:change={handleChange} />
+    </template>
+    
+    <style>
+        @import '../../node_modules/bytemd/dist/index.min.css';
+    </style>
